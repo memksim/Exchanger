d2c73081d15c76737c8a6b5e9857bfa97ca1e9be
memksim <memksim@vk.com>
2022-10-22 12:59:49 +0500

    add dashboard page & home page design

--- app/src/main/java/com/memksim/exchanger/ui/exchange/ExchangeViewModel.kt
+++ app/src/main/java/com/memksim/exchanger/ui/exchange/ExchangeViewModel.kt
@@ -1,10 +1,10 @@
-package com.memksim.exchanger.ui.notifications
+package com.memksim.exchanger.ui.exchange
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
 
-class NotificationsViewModel : ViewModel() {
+class ExchangeViewModel : ViewModel() {
 
     private val _text = MutableLiveData<String>().apply {
         value = "This is notifications Fragment"
